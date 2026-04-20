2ee6a03e4bb1182c15c520e94a760885ee68d9cf
Utej <91786744+utej8553@users.noreply.github.com>
2026-04-20 20:25:15 +0530

    added testing

--- testing/src/main/java/com/example/testing/entity/DemoEntity.java
+++ testing/src/main/java/com/example/testing/entity/DemoEntity.java
@@ -1,9 +1,18 @@
 package com.example.testing.entity;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "demoentity")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class DemoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
